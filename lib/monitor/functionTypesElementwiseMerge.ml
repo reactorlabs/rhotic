0a315f72fddd1055ba37e5f353727c27aec7ccfe
Ming-Ho Yee <mh@mhyee.com>
2021-05-27 14:25:38 -0400

    Function types monitor records return types

--- lib/monitor/functionTypesElementwiseMerge.ml
+++ lib/monitor/functionTypesElementwiseMerge.ml
@@ -43,13 +43,9 @@ let to_abstract_type = function
   | T_Int -> Int
   | T_Str -> Str
 
-(*
-- Record concrete types
-  - map of function ID to list of list of concrete types
-  - map of function ID to list of sets of concrete types
-- Record abstract types
-  ---> map of function ID to list of abstract types
-*)
+(* Map of function ID to abstract signature
+   The abstract signature is a list of abstract types, where the FIRST element is the return type 
+   and the rest of the list represents the parameter types, in order *)
 type abstract_function_types = abstract_type list FunTab.t
 
 class monitor =
@@ -58,24 +54,28 @@ class monitor =
 
     (* Update the entry for the called function.
        If this method is called, then the function must exist. *)
-    method record_call (_ : configuration) (_ : value) (id : identifier) (args : value list) : unit
-        =
+    method record_call (_ : configuration) (ret : value) (id : identifier) (args : value list)
+        : unit =
       let recorded_types = FunTab.find id recorded_functions in
-      let abstract_arg_types = List.map (vector_type %> to_abstract_type) args in
+      let abstract_arg_types =
+        (to_abstract_type @@ vector_type ret) :: List.map (vector_type %> to_abstract_type) args
+      in
       let new_types = List.map2 join_type recorded_types abstract_arg_types in
       recorded_functions <- FunTab.add id new_types recorded_functions
 
     (* Create an empty entry, with the args list initialized to Bot *)
     method record_fun_def (_ : configuration) (id : identifier) (params : identifier list) : unit =
-      let n = List.length params in
+      (* Add 1 for the return type *)
+      let n = List.length params + 1 in
       let init = List.init n (fun _ -> Bot) in
       recorded_functions <- FunTab.add id init recorded_functions
 
     method dump_table =
       let bindings = FunTab.to_seq recorded_functions in
-      let f (id, args) =
-        let output = List.map show_abstract_type args |> String.concat ", " in
-        Printf.printf "\tfunction %s: %s\n" id output in
+      let f (id, types) =
+        let type_str = List.map show_abstract_type types in
+        let ret, args = (List.hd type_str, List.tl type_str) in
+        Printf.printf "\t%s: (%s) -> %s\n" id (String.concat ", " args) ret in
       Stdlib.print_endline ">>> Dumping table of function types <<<" ;
       Seq.iter f bindings ;
       Stdlib.print_endline ">>> Finished <<<"
